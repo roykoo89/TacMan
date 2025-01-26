c21041c2c2d07d61c7059b7fae32165c79dc8f42
roy-augmentus <85816635+roykoo89@users.noreply.github.com>
2025-01-25 20:56:26 -0500

    Improve fake experienced

--- Assets/Scripts/VoxelCollision.cs
+++ Assets/Scripts/VoxelCollision.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,9 @@ public class VoxelCollision : MonoBehaviour
 {
     [SerializeField] private ExampleCommunicator _exampleCommunicator;
     [SerializeField] private AudioSource _audioSource;
-    [SerializeField] private ParticleSystem _particleSystem;
+    //[SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private GameObject VoxelDestroyedPrefab;
+    [SerializeField] private GameObject playerHeadset;
 
     private bool stopPlayEffects = false;
     
@@ -17,17 +20,41 @@ public class VoxelCollision : MonoBehaviour
             // Destroy or deactivate the voxel
             other.gameObject.SetActive(false);
 
+            // Instantiate a destroyed voxel at x times the amount but x time smaller
+            int amount = 2;
+            for(int i = 0; i < amount; i++)
+            {
+                GameObject destroyedVoxel = Instantiate(VoxelDestroyedPrefab, other.transform.position, Quaternion.identity);
+                destroyedVoxel.transform.localScale = other.transform.localScale/2;
+                //add force towards the player
+                destroyedVoxel.GetComponent<Rigidbody>().AddForce((playerHeadset.transform.position - destroyedVoxel.transform.position).normalized, ForceMode.Impulse);
+                Destroy(destroyedVoxel, 2f);
+            }
+            
             if (!stopPlayEffects)
             {
                 _audioSource.Play();
-                _particleSystem.gameObject.transform.position = other.transform.position;
-                _particleSystem.Play();
+                // _particleSystem.gameObject.transform.position = other.transform.position;
+                // _particleSystem.Play();
                 stopPlayEffects = true;
+                StartCoroutine(TriggerHapticFeedback());
                 StartCoroutine(ResetEffects());
             }
 
             _exampleCommunicator.SendMessageToServer("ResistMe");
         }
+    }
+    
+    private IEnumerator TriggerHapticFeedback()
+
+    {
+
+        OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
+
+        yield return new WaitForSeconds(0.3f);
+
+        OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
+
     }
     
     //coroutine to check every 3 seconds if there is no collision with any voxel, if so allow to play the sound and particle again
