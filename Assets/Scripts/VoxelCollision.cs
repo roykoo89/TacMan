242460a82f1fe76b46a3b0ffe8301af3434bdf74
roy-augmentus <85816635+roykoo89@users.noreply.github.com>
2025-01-25 19:46:48 -0500

    particle effect and sound to collision

--- Assets/Scripts/VoxelCollision.cs
+++ Assets/Scripts/VoxelCollision.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class VoxelCollision : MonoBehaviour
 {
     [SerializeField] private ExampleCommunicator _exampleCommunicator;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private ParticleSystem _particleSystem;
+
+    private bool stopPlayEffects = false;
     
     void OnTriggerEnter(Collider other)
     {
@@ -12,7 +16,24 @@ public class VoxelCollision : MonoBehaviour
         {
             // Destroy or deactivate the voxel
             other.gameObject.SetActive(false);
+
+            if (!stopPlayEffects)
+            {
+                _audioSource.Play();
+                _particleSystem.gameObject.transform.position = other.transform.position;
+                _particleSystem.Play();
+                stopPlayEffects = true;
+                StartCoroutine(ResetEffects());
+            }
+
             _exampleCommunicator.SendMessageToServer("ResistMe");
         }
+    }
+    
+    //coroutine to check every 3 seconds if there is no collision with any voxel, if so allow to play the sound and particle again
+    IEnumerator ResetEffects()
+    {
+        yield return new WaitForSeconds(0.5f);
+        stopPlayEffects = false;
     }
 }
